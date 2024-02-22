298efb819404e20f648dd1bb263e666c197e45d2
samie105 <samsonrichfield@gmail.com>
2024-02-22 12:32:39 +0100

    redeploy 2

--- app/signup/api/route.js
+++ app/signup/api/route.js
@@ -1,98 +1,98 @@
-import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken"; // Import jsonwebtoken
-import UserModel from "../../../mongodbConnect";
+// import { NextResponse } from "next/server";
+// import jwt from "jsonwebtoken"; // Import jsonwebtoken
+// import UserModel from "../../../mongodbConnect";
 
-export async function POST(request) {
-  const { name, country, email, dob, phoneNumber, password } =
-    await request.json();
-  const withdrawalPin = await generateUniquePin();
-  const taxCodePin = await generateUniquePin();
-  const lowerEmail = email.toLowerCase();
-  const Uppercountry = country.toUpperCase();
+// export async function POST(request) {
+//   const { name, country, email, dob, phoneNumber, password } =
+//     await request.json();
+//   const withdrawalPin = await generateUniquePin();
+//   const taxCodePin = await generateUniquePin();
+//   const lowerEmail = email.toLowerCase();
+//   const Uppercountry = country.toUpperCase();
 
-  const user = new UserModel({
-    name,
-    country: Uppercountry,
-    email: lowerEmail,
-    phone: phoneNumber,
-    dob,
-    password,
-    withdrawalPin,
-    taxCodePin,
-    lastProfit: 0,
-    autoTrades: true,
-    isVerified: false,
-    tradingBalance: 0, // Initialize the new number fields
-    totalDeposited: 0,
-    totalWithdrawn: 0,
-    totalAssets: 0,
-    trade: 0, // Initialize trade with 0
-    balance: 0,
-    totalWon: 0,
-    totalLoss: 0,
-    role: "user", // Set role as "user"
-    investmentPackage: "no plan",
-    notifications: [
-      {
-        id: crypto.randomUUID(),
-        method: "neutral",
-        type: "intro",
-        message: "Welcome to Live Markets",
-        date: new Date(),
-      },
-    ],
-    isReadNotifications: false,
-    isCopyTrading: false,
-    isLinkSeedPhrase: false,
-    isPaidTransactionFee: false,
-    isBanned: false,
-    planBonus: 0,
-    watchedCrypto: [],
-    stakings: [],
-    tradingProgress: 2,
-    trades: [],
-    tradersCopying: [],
-    paidStaking: Date.now(),
-    lastButtonClick: Date.now(),
-  });
-  try {
-    await user.save();
+//   const user = new UserModel({
+//     name,
+//     country: Uppercountry,
+//     email: lowerEmail,
+//     phone: phoneNumber,
+//     dob,
+//     password,
+//     withdrawalPin,
+//     taxCodePin,
+//     lastProfit: 0,
+//     autoTrades: true,
+//     isVerified: false,
+//     tradingBalance: 0, // Initialize the new number fields
+//     totalDeposited: 0,
+//     totalWithdrawn: 0,
+//     totalAssets: 0,
+//     trade: 0, // Initialize trade with 0
+//     balance: 0,
+//     totalWon: 0,
+//     totalLoss: 0,
+//     role: "user", // Set role as "user"
+//     investmentPackage: "no plan",
+//     notifications: [
+//       {
+//         id: crypto.randomUUID(),
+//         method: "neutral",
+//         type: "intro",
+//         message: "Welcome to Live Markets",
+//         date: new Date(),
+//       },
+//     ],
+//     isReadNotifications: false,
+//     isCopyTrading: false,
+//     isLinkSeedPhrase: false,
+//     isPaidTransactionFee: false,
+//     isBanned: false,
+//     planBonus: 0,
+//     watchedCrypto: [],
+//     stakings: [],
+//     tradingProgress: 2,
+//     trades: [],
+//     tradersCopying: [],
+//     paidStaking: Date.now(),
+//     lastButtonClick: Date.now(),
+//   });
+//   try {
+//     await user.save();
 
-    // Generate JWT token
-    const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
-      expiresIn: "5d",
-    });
-    const role = await user.role;
+//     // Generate JWT token
+//     const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+//       expiresIn: "5d",
+//     });
+//     const role = await user.role;
 
-    return NextResponse.json(
-      { success: true, email, token, role },
-      { status: 201 }
-    );
-  } catch (error) {
-    return NextResponse.json({ success: false, error }, { status: 400 });
-  }
-}
+//     return NextResponse.json(
+//       { success: true, email, token, role },
+//       { status: 201 }
+//     );
+//   } catch (error) {
+//     return NextResponse.json({ success: false, error }, { status: 400 });
+//   }
+// }
 
-async function generateUniquePin() {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-#$";
-  let pin;
+// async function generateUniquePin() {
+//   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-#$";
+//   let pin;
 
-  do {
-    pin = "";
-    for (let i = 0; i < 8; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      pin += characters.charAt(randomIndex);
-    }
-  } while (await isPinExists(pin));
+//   do {
+//     pin = "";
+//     for (let i = 0; i < 8; i++) {
+//       const randomIndex = Math.floor(Math.random() * characters.length);
+//       pin += characters.charAt(randomIndex);
+//     }
+//   } while (await isPinExists(pin));
 
-  return pin;
-}
+//   return pin;
+// }
 
-async function isPinExists(pin) {
-  // Query the database to see if the pin already exists in either the withdrawalPin or taxCodePin fields
-  const userWithWithdrawalPin = await UserModel.findOne({ withdrawalPin: pin }); // Use UserModel
-  const userWithTaxCodePin = await UserModel.findOne({ taxCodePin: pin }); // Use UserModel
+// async function isPinExists(pin) {
+//   // Query the database to see if the pin already exists in either the withdrawalPin or taxCodePin fields
+//   const userWithWithdrawalPin = await UserModel.findOne({ withdrawalPin: pin }); // Use UserModel
+//   const userWithTaxCodePin = await UserModel.findOne({ taxCodePin: pin }); // Use UserModel
 
-  // Return true if either query finds a user, false otherwise
-  return Boolean(userWithWithdrawalPin || userWithTaxCodePin);
-}
+//   // Return true if either query finds a user, false otherwise
+//   return Boolean(userWithWithdrawalPin || userWithTaxCodePin);
+// }
--- mongodbConnect.js
+++ mongodbConnect.js
@@ -1,69 +1,69 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const mongoURI = process.env.CONNECTION_STRING;
+const mongoURI = process.env.CONNECTION_STRING;
 
-// // Establish the connection
-// mongoose.connect(mongoURI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// Establish the connection
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// // Log when successfully connected
-// mongoose.connection.on("connected", () => {
-//   console.log("Connected to MongoDB");
-// });
+// Log when successfully connected
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
 
-// // Log any errors during connection
-// mongoose.connection.on("error", (err) => {
-//   console.log("Error connecting to MongoDB:", err);
-// });
+// Log any errors during connection
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to MongoDB:", err);
+});
 
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   country: String,
-//   dob: String,
-//   email: { type: String, unique: true },
-//   phone: String,
-//   password: String,
-//   depositHistory: [Object],
-//   withdrawalHistory: [Object],
-//   withdrawalPin: { type: String, unique: true },
-//   taxCodePin: { type: String, unique: true },
-//   autoTrades: Boolean,
-//   isVerified: Boolean,
-//   verificationCode: String,
-//   lastProfit: Number,
-//   codeExpiry: Date,
-//   emailVerified: { type: Boolean, default: false },
-//   tradingBalance: Number, // New field: trading balance
-//   totalDeposited: Number, // New field: total deposited
-//   totalWithdrawn: Number, // New field: total withdrawn
-//   totalAssets: Number, // New field: total assets
-//   trade: Number, // New field: trade
-//   balance: Number, // New field: balance
-//   totalWon: Number, // New field: total won
-//   totalLoss: Number, // New field: total loss
-//   role: String, // New field: role
-//   investmentPackage: String, // New field: investment package
-//   notifications: [Object],
-//   isReadNotifications: Boolean,
-//   isCopyTrading: Boolean,
-//   tradersCopying: [Object],
-//   isLinkSeedPhrase: Boolean,
-//   seedPhrases: [Object],
-//   isPaidTransactionFee: Boolean,
-//   latestTrades: [Object],
-//   isBanned: Boolean,
-//   planBonus: Number,
-//   watchedCrypto: [Object],
-//   stakings: [Object],
-//   trades: [Object],
-//   tradingProgress: Number,
-//   paidStaking: { type: Date, default: Date.now, required: true },
-//   lastButtonClick: Date,
-// });
+const userSchema = new mongoose.Schema({
+  name: String,
+  country: String,
+  dob: String,
+  email: { type: String, unique: true },
+  phone: String,
+  password: String,
+  depositHistory: [Object],
+  withdrawalHistory: [Object],
+  withdrawalPin: { type: String, unique: true },
+  taxCodePin: { type: String, unique: true },
+  autoTrades: Boolean,
+  isVerified: Boolean,
+  verificationCode: String,
+  lastProfit: Number,
+  codeExpiry: Date,
+  emailVerified: { type: Boolean, default: false },
+  tradingBalance: Number, // New field: trading balance
+  totalDeposited: Number, // New field: total deposited
+  totalWithdrawn: Number, // New field: total withdrawn
+  totalAssets: Number, // New field: total assets
+  trade: Number, // New field: trade
+  balance: Number, // New field: balance
+  totalWon: Number, // New field: total won
+  totalLoss: Number, // New field: total loss
+  role: String, // New field: role
+  investmentPackage: String, // New field: investment package
+  notifications: [Object],
+  isReadNotifications: Boolean,
+  isCopyTrading: Boolean,
+  tradersCopying: [Object],
+  isLinkSeedPhrase: Boolean,
+  seedPhrases: [Object],
+  isPaidTransactionFee: Boolean,
+  latestTrades: [Object],
+  isBanned: Boolean,
+  planBonus: Number,
+  watchedCrypto: [Object],
+  stakings: [Object],
+  trades: [Object],
+  tradingProgress: Number,
+  paidStaking: { type: Date, default: Date.now, required: true },
+  lastButtonClick: Date,
+});
 
-// const UserModel =
-//   mongoose.models.UserShalom || mongoose.model("UserShalom", userSchema);
+const UserModel =
+  mongoose.models.UserShalom || mongoose.model("UserShalom", userSchema);
 
-// export default UserModel;
+export default UserModel;
